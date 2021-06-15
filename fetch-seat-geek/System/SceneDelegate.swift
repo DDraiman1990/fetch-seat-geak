588405e9f0da63da4f3166c1e929aa06c6c6fdc1
DDraiman1990 <ddraiman1990@gmail.com>
2021-06-15 09:08:35 -0500

    Generic tab controller

--- fetch-seat-geek/System/SceneDelegate.swift
+++ fetch-seat-geek/System/SceneDelegate.swift
@@ -16,7 +16,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             fatalError("Unexpected scene type")
         }
         self.window = UIWindow(windowScene: scene)
-        setRoot(viewController: UIViewController())
+        
+        let vc1 = UIViewController()
+        vc1.view.backgroundColor = .red
+        let vc2 = UIViewController()
+        vc2.view.backgroundColor = .green
+        let vc3 = UIViewController()
+        vc3.view.backgroundColor = .blue
+        setRoot(viewController: MainTabController(tabs: [
+            .init(
+                viewController: vc1,
+                tabTitle: "VC1",
+                tabDeselectedIcon: UIImage(systemName: "bubble.right.fill", withConfiguration: UIImage.SymbolConfiguration(weight: .regular)),
+                tabSelectedIcon: UIImage(systemName: "ellipses.bubble.fill", withConfiguration: UIImage.SymbolConfiguration(weight: .regular))),
+            .init(
+                viewController: vc2,
+                tabTitle: "VC2",
+                tabDeselectedIcon: UIImage(systemName: "bubble.right.fill", withConfiguration: UIImage.SymbolConfiguration(weight: .regular)),
+                tabSelectedIcon: UIImage(systemName: "ellipses.bubble.fill", withConfiguration: UIImage.SymbolConfiguration(weight: .regular))),
+            .init(
+                viewController: vc3,
+                tabTitle: "VC3",
+                tabDeselectedIcon: UIImage(systemName: "bubble.right.fill", withConfiguration: UIImage.SymbolConfiguration(weight: .regular)),
+                tabSelectedIcon: UIImage(systemName: "ellipses.bubble.fill", withConfiguration: UIImage.SymbolConfiguration(weight: .regular)))
+        ]))
     }
     
     private func setRoot(
