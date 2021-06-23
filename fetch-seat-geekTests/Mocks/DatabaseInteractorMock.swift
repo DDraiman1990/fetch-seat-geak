63eef07a12cf5ca74df8303b1213f65cd4db4342
DDraiman1990 <ddraiman1990@gmail.com>
2021-06-23 15:36:13 -0500

    TrackedManagerTests

--- fetch-seat-geekTests/Mocks/DatabaseInteractorMock.swift
+++ fetch-seat-geekTests/Mocks/DatabaseInteractorMock.swift
@@ -9,24 +9,33 @@ import Foundation
 import RxSwift
 @testable import fetch_seat_geek
 
-//
-//final class DatabaseInteractorMock: DatabaseInteracting {
-//    var throwError: Error?
-//    var returnValue: String = ""
-//    var storedValue: Any?
-//    var getValue: Any?
-//
-//    func store<T>(_ value: T, forKey key: String) throws where T : Decodable, T : Encodable {
-//        if let error = throwError {
-//            throw error
-//        }
-//        self.storedValue = value
-//    }
-//
-//    func get<T>(key: String) throws -> T where T : Decodable, T : Encodable {
-//        if let error = throwError {
-//            throw error
-//        }
-//        return T.init(from: MockDecoder())
-//    }
-//}
+final class DatabaseInteractorMock: DatabaseInteracting {
+    var throwError: Error?
+    var returnValue: String = ""
+    var storedData: Data?
+    var getData: Data?
+    
+    init<T: Codable>(storedData: T?) {
+        if let data = storedData {
+            setStoredData(from: data)
+        }
+    }
+    
+    func setStoredData<T: Codable>(from model: T) {
+        self.storedData = try? JSONEncoder().encode(model)
+    }
+    
+    func store<T>(_ value: T, forKey key: String) throws where T : Decodable, T : Encodable {
+        if let error = throwError {
+            throw error
+        }
+        self.storedData = try? JSONEncoder().encode(value)
+    }
+    
+    func get<T>(key: String) throws -> T where T : Decodable, T : Encodable {
+        if let error = throwError {
+            throw error
+        }
+        return try! JSONDecoder().decode(T.self, from: storedData!)
+    }
+}
--- fetch-seat-geekTests/Tests/TrackedManagerTests.swift
+++ fetch-seat-geekTests/Tests/TrackedManagerTests.swift
@@ -14,20 +14,124 @@ import RxSwift
 class TrackedManagerTests: QuickSpec {
     
     override func spec() {
+        let testId = 123
         var disposeBag = DisposeBag()
         var mockLogger: LoggerMock!
         var sut: TrackedManager!
+        var mockDatabaseInteractor: DatabaseInteractorMock!
         
-        beforeEach {
+        
+        func createSut(initialData: Set<Int> = .init()) {
             disposeBag = .init()
-            mockLogger = LoggerMock()
-            
+            mockLogger = .init()
+            mockDatabaseInteractor = .init(storedData: initialData)
+            sut = .init(
+                databaseInteractor: mockDatabaseInteractor,
+                logger: mockLogger)
         }
         
         describe("Tracked changes") {
-            context("Given event added") {
-                it("should send onTrackedChanged event") {
-                    
+            context("Given id is not tracked") {
+                beforeEach {
+                    createSut()
+                    expect(sut.trackedIds)
+                        .toNot(contain(testId))
+                }
+                it("should send onTrackedChanged event when added") {
+                    sut
+                        .onTrackedChanged
+                        .skip(1)
+                        .subscribeToValue({ ids in
+                            expect(ids)
+                                .toEventually(contain(testId))
+                        })
+                        .disposed(by: disposeBag)
+                    sut
+                        .addToTracked(id: testId)
+                        .subscribeToResult { _ in}
+                        .disposed(by: disposeBag)
+                }
+                it("should add the id to the trackedIds when added") {
+                    sut
+                        .addToTracked(id: testId)
+                        .subscribeToResult { result in
+                            switch result {
+                            case .success:
+                                expect(sut.trackedIds)
+                                    .toEventually(contain(testId))
+                            case .failure:
+                                fail()
+                            }
+                        }
+                        .disposed(by: disposeBag)
+                }
+                it("should add the id if toggled") {
+                    sut
+                        .toggleTracked(id: testId)
+                        .subscribeToResult { result in
+                            switch result {
+                            case .success(let isTracked):
+                                expect(sut.trackedIds)
+                                    .toEventually(contain(testId))
+                                expect(isTracked)
+                                    .toEventually(beTrue())
+                            case .failure:
+                                fail()
+                            }
+                        }
+                        .disposed(by: disposeBag)
+                }
+            }
+            
+            context("Given id is already tracked") {
+                beforeEach {
+                    createSut(initialData: [testId])
+                    expect(sut.trackedIds)
+                        .to(contain(testId))
+                }
+                it("should send onTrackedChanged event when removed") {
+                    sut
+                        .onTrackedChanged
+                        .skip(1)
+                        .subscribeToValue({ ids in
+                            expect(ids)
+                                .toEventuallyNot(contain(testId))
+                        })
+                        .disposed(by: disposeBag)
+                    sut
+                        .removeFromTracked(id: testId)
+                        .subscribeToResult { _ in}
+                        .disposed(by: disposeBag)
+                }
+                it("should remove id from trackedIds when removed") {
+                    sut
+                        .removeFromTracked(id: testId)
+                        .subscribeToResult { result in
+                            switch result {
+                            case .success:
+                                expect(sut.trackedIds)
+                                    .toEventuallyNot(contain(testId))
+                            case .failure:
+                                fail()
+                            }
+                        }
+                        .disposed(by: disposeBag)
+                }
+                it("should remove id when toggled") {
+                    sut
+                        .toggleTracked(id: testId)
+                        .subscribeToResult { result in
+                            switch result {
+                            case .success(let isTracked):
+                                expect(sut.trackedIds)
+                                    .toEventuallyNot(contain(testId))
+                                expect(isTracked)
+                                    .toEventually(beFalse())
+                            case .failure:
+                                fail()
+                            }
+                        }
+                        .disposed(by: disposeBag)
                 }
             }
         }
